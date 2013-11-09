eb13eb7a5ff2e34ccecb9d67f4669f62e0bc958a
Tomas Dohnalek <dohnto@gmail.com>
2013-11-09 12:10:29 +0100

    First shop.

--- src/pso/Particle.java
+++ src/pso/Particle.java
@@ -37,4 +37,58 @@ public class Particle {
     public static void setW(double w) {
         Particle.w = w;
     }
+    
+    /**
+     * position(t+1) = position(t) + velocity(t+1).
+     * Be sure that you called updateVelocity first!
+     */
+    public void updatePosition() {
+    	position = sumLists(position, velocity);
+    }
+    
+    /**
+     * velocity(t+1) = velocity(t) + c1 * r1 * (bestPosition(t) - position(t)) + c2 * r2 * (bestGlobalPosition(t) - position(t))
+     */
+    public void updateVelocity() {
+    	
+    	ArrayList<Double> pTxT = subtractLists(bestParticlePosition, position);
+    	ArrayList<Double> gTxT = subtractLists(bestGlobalPosition, position);
+    	
+    	ArrayList<Double> c1r1pTxT = multiplyList(c1*r1, pTxT);
+    	ArrayList<Double> c2r2pTxT = multiplyList(c2*r2, gTxT);
+ 
+    	ArrayList<Double> vTc1r1pTxT = sumLists(velocity, c1r1pTxT);
+    	ArrayList<Double> vTc1r1pTxTc2r2pTxT = sumLists(vTc1r1pTxT, c2r2pTxT);
+    	
+    	velocity = vTc1r1pTxTc2r2pTxT;
+    }
+    
+    public static ArrayList<Double> subtractLists(final ArrayList<Double> a, final ArrayList<Double> b) {
+    	ArrayList<Double> retval = new ArrayList<Double>();
+    	if (a.size() != b.size())
+    		throw new ArrayIndexOutOfBoundsException("");
+    	for (int i = 0; i < a.size(); i++) {
+    		retval.add(a.get(i) - b.get(i));
+    	}
+    	return retval;
+    }
+    
+    public static ArrayList<Double> sumLists(final ArrayList<Double> a, final ArrayList<Double> b) {
+    	ArrayList<Double> retval = new ArrayList<Double>();
+    	if (a.size() != b.size())
+    		throw new ArrayIndexOutOfBoundsException("");
+    	for (int i = 0; i < a.size(); i++) {
+    		retval.add(a.get(i) + b.get(i));
+    	}
+    	return retval;
+    }
+    
+    public static ArrayList<Double> multiplyList(final double c, final ArrayList<Double> list) {
+    	ArrayList<Double> retval = new ArrayList<Double>();
+    	for (int i = 0; i < list.size(); i++) {
+    		retval.add(c * list.get(i));
+    	}
+    	return retval;
+    }
+    
 }
--- src/pso/ParticleSwarmOptimization.java
+++ src/pso/ParticleSwarmOptimization.java
@@ -1,11 +1,47 @@
 package pso;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ParticleSwarmOptimization {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-    }
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		if (args.length != 1) { //
+			usage("ParticleSwarmOptimization");
+			return;
+		}
+
+		Configuration config;
+		try {
+			config = new Configuration(args[0]);
+		} catch (NoSuchFieldException | IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		FitnessFunction ff = new FitnessFunction() {
+			@Override
+			public double get(ArrayList<Double> vector) {
+				double sum = 0;
+				for (Double value : vector)
+					sum += value * value;
+				return sum;
+			}
+		};
+
+		new Solver(ff, config.maxIterations, config.dimension, config.epsilon,
+				config.rounds, config.inertiaWeightStart,
+				config.inertiaWeightEnd, config.connections, config.c1,
+				config.c2);
+	}
+
+	public static void usage(String progname) {
+		System.out.println("Usage: " + progname + " CONFIG_FILE");
+		System.out
+				.println("Solves problem using particle swarm optimalization.");
+	}
 }
