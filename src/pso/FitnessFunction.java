757656de564e6e2446b44778682a86bdc525e34b
Jan Bednarik <ja.bedna1@gmail.com>
2013-11-11 00:09:49 +0100

    factory pattern

--- src/pso/FitnessFunction.java
+++ src/pso/FitnessFunction.java
@@ -4,4 +4,10 @@ import java.util.ArrayList;
 
 public abstract class FitnessFunction {
 	public abstract double get(final ArrayList<Double> vector);
+        
+        /**
+         * Subclasses must be able to create the objects of themselves.
+         * @return new instance
+         */
+        public abstract FitnessFunction createFitnessFunction();        
 }
--- src/pso/ParticleSwarmOptimization.java
+++ src/pso/ParticleSwarmOptimization.java
@@ -2,47 +2,51 @@ package pso;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ParticleSwarmOptimization {
 
-	/**
-	 * @param args
-	 *            the command line arguments
-	 */
-	public static void main(String[] args) {
-		if (args.length != 1) { //
-			usage("ParticleSwarmOptimization");
-			return;
-		}
+    static {
+        try {
+            /* load all necessary classes */
+            Class.forName("pso.CircleProblem");
+            Class.forName("pso.KnapsackProblem");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ParticleSwarmOptimization.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-		Configuration config;
-		try {
-			config = new Configuration(args[0]);
-		} catch (NoSuchFieldException | IOException e) {
-			e.printStackTrace();
-			return;
-		}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 1) { //
+            usage("ParticleSwarmOptimization");
+            return;
+        }
 
-		FitnessFunction ff = new FitnessFunction() {
-			@Override
-			public double get(ArrayList<Double> vector) {
-				double sum = 0;
-				for (Double value : vector) {
-					sum += value * value;
-				}
-				return sum;
-			}
-		};
+        Configuration config;
+        try {
+            config = new Configuration(args[0]);
+        } catch (NoSuchFieldException | IOException e) {
+            e.printStackTrace();
+            return;
+        }
 
-		new Solver(ff, config.maxIterations, config.dimension, config.epsilon,
-				config.inertiaWeightStart,
-				config.inertiaWeightEnd, config.connections, config.c1,
-				config.c2).solve();
-	}
+        /* Creation of the factory */
+        FitnessFunctionFactory ffFactory = FitnessFunctionFactory.getInstance();
 
-	public static void usage(String progname) {
-		System.out.println("Usage: " + progname + " CONFIG_FILE");
-		System.out.println("Solves problem using particle swarm optimalization.");
-	}
-	
+        FitnessFunction ff = ffFactory.createFitnessFunction("circle");
+
+        new Solver(ff, config.maxIterations, config.dimension, config.epsilon,
+                config.inertiaWeightStart,
+                config.inertiaWeightEnd, config.connections, config.c1,
+                config.c2).solve();
+    }
+
+    public static void usage(String progname) {
+        System.out.println("Usage: " + progname + " CONFIG_FILE");
+        System.out.println("Solves problem using particle swarm optimalization.");
+    }
 }
