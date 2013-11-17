240deec815ae2b4ab7f0a7db4f68dee8496e5a21
Jan Bednarik <ja.bedna1@gmail.com>
2013-11-17 21:00:06 +0100

    fitness function kanpsack

--- src/pso/KnapsackProblem.java
+++ src/pso/KnapsackProblem.java
@@ -7,7 +7,6 @@ import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-
 /**
  * This class implements the fitness function evaluating the knapsack problem.
  */
@@ -17,35 +16,41 @@ public class KnapsackProblem extends FitnessFunction {
         /* Registers itself in the factory. */
         FitnessFunctionFactory.registerFitnessFunction("knapsack", new KnapsackProblem());
     }
-    
-    private static final double clampMin = 
+    private static final double clampMin =
             CommonConstants.knapsackProblemClampVelocityMin;
-    private static final double clampMax = 
+    private static final double clampMax =
             CommonConstants.knapsackProblemClampVelocityMax;
     private ArrayList<Package> packages;
     private double weightLimit;
 
-
     public KnapsackProblem() {
-        super(CommonConstants.knapsackProblemParticlePositionMin, 
-              CommonConstants.knapsackProblemParticlePositionMax,
-              clampMin, clampMax);
+        super(CommonConstants.knapsackProblemParticlePositionMin,
+                CommonConstants.knapsackProblemParticlePositionMax,
+                clampMin, clampMax);
         packages = new ArrayList<>();
     }
-   
+
     public void setWeightLimit(double weightLimit) {
         this.weightLimit = weightLimit;
     }
-    
+
     /**
      * Implementation of fitness function.
      *
-     * @param vector
+     * @param vector particle's current position
      * @return fitness value
      */
     @Override
     public double get(ArrayList<Double> vector) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        double fitness;
+
+        if ((weightLimit - knapsackQuality(vector, Package.PackageAttributes.WEIGHT)) < 0.0) {
+            fitness = Double.MAX_VALUE;
+        } else {
+            fitness = Double.MAX_VALUE - knapsackQuality(vector, Package.PackageAttributes.VALUE);
+        }
+
+        return fitness;
     }
 
     /**
@@ -149,55 +154,72 @@ public class KnapsackProblem extends FitnessFunction {
     @Override
     public ArrayList<Double> initParticlePosition(int dimension) {
         double maxPackageWeight = getMaxPackageWeight();
-        int    minFitPackages   = (int) (weightLimit / maxPackageWeight);
-        int    i;
-        
+        int minFitPackages = (int) (weightLimit / maxPackageWeight);
+        int i;
+
         ArrayList<Double> position = new ArrayList<>(dimension);
-        for(i = 0; i < dimension; i++) {
+        for (i = 0; i < dimension; i++) {
             position.add(0.0);
         }
-        
+
         i = 0;
-        while(i < minFitPackages) {
-            int index = (int) getRandomNumber(0, dimension - Double.MIN_VALUE);            
-            if(position.get(index) != 0.0) {
+        while (i < minFitPackages) {
+            int index = (int) getRandomNumber(0, dimension - Double.MIN_VALUE);
+            if (position.get(index) != 0.0) {
                 continue;
-            }            
+            }
             position.set(index, 1.0);
             i++;
         }
-        
+
         return position;
     }
 
     @Override
     public ArrayList<Double> clampVelocity(ArrayList<Double> velocity) {
         int sign;
-        
+
         for (int i = 0; i < velocity.size(); i++) {
             if (velocity.get(i) < clampMin) {
                 velocity.set(i, clampMin);
             } else if (velocity.get(i) > clampMax) {
                 velocity.set(i, clampMax);
             }
-            
+
             // Map to range <-1, 1> using sigmoid function        
-            sign = (velocity.get(i) > 0.0) ? 1 : -1;            
+            sign = (velocity.get(i) > 0.0) ? 1 : -1;
             velocity.set(i, sign * sigmoid(velocity.get(i)));
-        }        
-        
+        }
+
         return velocity;
-    }    
-    
+    }
+
     /**
      * Implements sigmoid function
+     *
      * @param arg
-     * @return 
+     * @return
      */
     private double sigmoid(double arg) {
-        return 1/(1 + Math.exp(-arg));
+        return 1 / (1 + Math.exp(-arg));
+    }
+
+    /**
+     * Counts current knapsack weight.
+     *
+     * @param position
+     * @return
+     */
+    private double knapsackQuality(ArrayList<Double> position,
+            Package.PackageAttributes a) {
+        double sum = 0.0;
+        for (int i = 0; i < position.size(); i++) {
+            sum += position.get(i) * packages.get(i).get(a);
+        }
+        return sum;
     }
 }
+
 /**
  * Excpetion thrown if the file specifing the packages properties is empty.
  */
