a8c8ddae6714a6512e843b28b7ea0a0b1bfc39ce
Tomas Dohnalek <dohnto@gmail.com>
2013-11-24 15:37:08 +0100

    CS.

---
+++ src/pso/CircleSolver.java
@@ -0,0 +1,19 @@
+package pso;
+
+public class CircleSolver extends Solver {
+
+	public CircleSolver(Problem problem, int maxIterations, int dimension,
+			double epsilon, double inertiaWeightStart, double inertiaWeightEnd,
+			int connections, double c1, double c2) {
+		super(problem, maxIterations, dimension, epsilon, inertiaWeightStart,
+				inertiaWeightEnd, connections, c1, c2);
+		int particleCount = 10 + (int) (10 * Math.sqrt(dimension));
+        for (int i = 0; i < particleCount; i++) {
+            particles.add(new Particle(dimension));
+        }
+        
+        // find the best global position and set it to all of them
+        updateBestGlobalPosition(connections);
+	}
+
+}
